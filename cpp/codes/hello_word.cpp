797ceebcf8004e7de5879fd0185b9d70648b21e8
dengshizhe <dengshizhe96@outlook.com>
2023-08-03 00:00:17 +0800

    测试回调函数

--- cpp/codes/hello_word.cpp
+++ cpp/codes/hello_word.cpp
@@ -3,10 +3,21 @@
 //
 
 #include <iostream>
-
+#include <string>
 
 int main()
 {
-    std::cout << "Hello World!" << std::endl;
+    std::string name;
+    std::cout << "please input your name" << std::endl;
+    std::cin >> name;
+    std::cout << "Hello Wdeorld! " << name << std::endl;
+    std::string previous = "";
+    std::string current;
+    while(std::cin>>current) {
+        if(previous == current) {
+            std::cout << "repeated word: " << current << std::endl;
+        }
+        previous = current;
+    }
     return 0;
 }
