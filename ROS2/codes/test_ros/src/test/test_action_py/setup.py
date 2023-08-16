e177740f9ce6950cdde78f7598b78eb7f31e8311
dengshizhe <dengshizhe96@outlook.com>
2023-08-16 13:09:27 +0800

    test action python

---
+++ ROS2/codes/test_ros/src/test/test_action_py/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+package_name = 'test_action_py'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='dsz',
+    maintainer_email='dengshizhe96@outlook.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'action_move_client = test_action_py.action_move_client:main',
+            'action_move_server = test_action_py.action_move_server:main',
+        ],
+    },
+)
